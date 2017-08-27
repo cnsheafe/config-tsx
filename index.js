2d917188c4607d735407370265670df85ff141fe
cnsheafe <cnsheafe@gmail.com>
2017-08-27 15:11:00 -0600

    Changed path resolution
    Path was being reference from node_modules
    instead of the project root directory.
    Fixed by taking a given projectRoot.

--- index.js
+++ index.js
@@ -2,27 +2,38 @@ const fs = require("fs");
 const path = require("path");
 
 // Paths used for webpack and tsc. Change to suit your app.
-const clientRoot = "app";
-const entryFile = path.resolve(__dirname, `${clientRoot}/src/index.tsx`);
-const inputDir = path.resolve(__dirname, clientRoot);
-const outputDir = path.resolve(__dirname, `${clientRoot}/dist`);
+function createPaths(projectRoot, options=undefined) {
 
-const  appPaths = {
-    "client-root": clientRoot,
-    "entry-file": entryFile,
-    "input-dir": inputDir,
-    "output-dir": outputDir
-};
+    if (typeof options === "undefined") {
+        clientRoot = "app";
+        options = {
+            "client-root": clientRoot,
+            "entry-file": path.posix.normalize(
+                `${projectRoot}/${clientRoot}/src/index.tsx`
+            ),
+            "input-dir": path.posix.normalize(
+                `${projectRoot}/${clientRoot}`
+            ),
+            "output-dir": path.posix.normalize(
+                `${projectRoot}/${clientRoot}/dist`
+            )
+        }
+    }
 
-function createTsConfig() {
+    return options;
+}
+
+
+function createTsConfig(projectRoot, outDir) {
 
     const stream = fs.createWriteStream(
-        path.resolve(__dirname, "tsconfig.json")
+        path.posix.normalize(`${projectRoot}/tsconfig.json`)
     );
+
     const config = JSON.stringify(
         {
             compilerOptions: {
-                outDir: appPaths.outDir,
+                outDir: outDir,
                 sourceMap: true,
                 module: "commonjs",
                 target: "es2015",
@@ -33,15 +44,16 @@ function createTsConfig() {
                 ]
             },
             exclude: [
-                path.resolve(__dirname, "node_modules")
+                path.posix.normalize(`${projectRoot}/node_modules`)
             ]
         }
     );
+
     stream.write(config);
     stream.close();
 }
 
 module.exports = {
     createTsConfig: createTsConfig,
-    appPaths: appPaths
+    createPaths: createPaths
 };
